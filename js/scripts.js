4b54606af3cbc8c3599fa3535456ab96b663915a
Brian Hensley, Justin Kerntz and Nathan Aden <nathan@>
2019-04-10 16:13:51 -0700

    add comments to the js for clarity

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 //Backend Logic
+
+// This is a constructor to create the Game
 function Game() {
   this.board = new Board();
   this.players = [];
@@ -6,20 +8,26 @@ function Game() {
   this.currentPlayer = 0;
 }
 
+// This prototype changes the players turn
 Game.prototype.getNextPlayer = function () {
   this.currentPlayer++;
-  if (this.currentPlayer > this.players.length - 1) {
+  if (this.currentPlayer > 1) {
+      // This basically says take the players array position (0 or 1) add 1, then check if the new total is more than 1 (this.players.length = 2, then it subtracts 1). If it is not higher than 1 it returns the the player position.  If it is higher than 1 it is reset to 0, then returns that position.  Making it an insane way of turning 1's to 0's and 0's to 1's.
+  // if (this.currentPlayer > this.players.length - 1) {
     this.currentPlayer = 0;
   }
   return this.players[this.currentPlayer];
 };
 
+// This is the Board constructor that creates an empty array and a place for the winner.
 function Board() {
   this.spaces = [],
   this.gameWinner = ""
 }
 
+
 Board.prototype.getSpaceValue = function (squareId) {
+  // $('#reset-game').hide();
   if (this.spaces[squareId]) {
     return this.spaces[squareId]
   }
@@ -35,6 +43,20 @@ Board.prototype.markSquare = function (squareId, oTurn) {
   }
 };
 
+Board.prototype.checkWin = function () {
+  var s = this.spaces;
+  if ((s[0] && (s[0] === s[1] && s[1] === s[2])) ||
+  (s[3] && (s[3] === s[4] && s[4] === s[5])) ||
+  (s[6] && (s[6] === s[7] && s[7] === s[8])) ||
+  (s[0] && (s[0] === s[3] && s[3] === s[6])) ||
+  (s[1] && (s[1] === s[4] && s[4] === s[7])) ||
+  (s[2] && (s[2] === s[5] && s[5] === s[8])) ||
+  (s[0] && (s[0] === s[4] && s[4] === s[8])) ||
+  (s[2] && (s[2] === s[4] && s[4] === s[6]))) {
+    return true;
+  }
+};
+
 function Player(name, color) {
   if (!color){
     color = randomcolorS();
@@ -42,21 +64,6 @@ function Player(name, color) {
   this.name = name;
   this.color = color;
 }
-
-Board.prototype.checkWin = function () {
-
-  var s = this.spaces;
-  if ((s[0] && (s[0] === s[1] && s[1] === s[2])) ||
-      (s[3] && (s[3] === s[4] && s[4] === s[5])) ||
-      (s[6] && (s[6] === s[7] && s[7] === s[8])) ||
-      (s[0] && (s[0] === s[3] && s[3] === s[6])) ||
-      (s[1] && (s[1] === s[4] && s[4] === s[7])) ||
-      (s[2] && (s[2] === s[5] && s[5] === s[8])) ||
-      (s[0] && (s[0] === s[4] && s[4] === s[8])) ||
-      (s[2] && (s[2] === s[4] && s[4] === s[6]))) {
-    return true;
-  }
-};
 
 function changePlayer(game, outputTable) {
   var player = game.getNextPlayer();
@@ -71,49 +78,55 @@ function changePlayer(game, outputTable) {
   updateBoard(game.board, outputTable);
 }
 
-
-
 //UI Logic
 function addEventHandlers(game, outputTable) {
   $("#board").on("click", "th", function() {
     var itemId = this.id
-
+//  This part checks is a clicked box already has a value, and if there is no winner yet.
     if (game.board.getSpaceValue(parseInt(itemId)) === "&nbsp;" &&
-        game.board.gameWinner === "") {
-      console.log("works");
+    game.board.gameWinner === "") {
       game.board.markSquare(itemId, game.oTurn);
 
+// This part checks the board for winners after every click
       if (game.board.checkWin()) {
+//  If it's O's turn and there is a winner this is displayed
         if (game.oTurn) {
           game.board.gameWinner = "O"
           $('#gameWinner').text("O Wins!")
           $('#gameWinner').show("fade", 1000)
         } else {
+// If it's not O's turn and there is a winner X wins is displayed
           game.board.gameWinner = "X"
           $('#gameWinner').text("X Wins!")
           $('#gameWinner').show("fade", 1000)
         }
       }
+// If there is no winner it runs the changePlayer funciton
       changePlayer(game, outputTable);
     }
   });
 }
 
+// This function runs when clicking Play Again, it fixes the error where previous games X's and O's stayed on the board after starting new game.
 function removeEvent() {
   $("#board").off("click", "th");
 }
 
+// This function recreates the board every turn with X's and O's in place
 function updateBoard(board, outputTable) {
   var items = ""
   for (var i = 0; i < 9; i++) {
+// these are the Space Id's where to begin new lines on the grid.
     if (i === 0 || i === 3 || i === 6) {
       items += "<tr>"
     }
     items += "<th id='" + i + "'>" + board.getSpaceValue(i) + "</th>"
+// this part ends a row when it gets to space value 2, 5, 8
     if (i === 2 || i === 5 || i === 8) {
       items += "</tr>"
     }
   }
+// This is the function to empty the board and immediately recreate it.
   $(outputTable).empty();
   $(outputTable).html(items);
 }
@@ -131,12 +144,11 @@ $(document).ready(function () {
     myGame.players.push(new Player("Player 1"))
     myGame.players.push(new Player("Player 2"))
     $('#gameWinner').hide("fade", 1000)
+    $('#players').text("Player 1");
     removeEvent();
     addEventHandlers(myGame, "#board");
     updateBoard(myGame.board, "#board")
   });
-
-
 })
 
 var randomcolor = function() {
@@ -149,7 +161,6 @@ var randomcolor = function() {
 
 var randomcolorS = function() {
   var h = Math.round( Math.random() * 360);
-
   var color = "hsl(" + h + ", 50%, 80%)"
   //hsl(360, 100%, 100%);
   console.log(color);
